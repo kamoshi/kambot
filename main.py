3dfe39772c967ca56fb4a68e664782fbdef8ed29
kamoshi <18511281+kamoshi@users.noreply.github.com>
2020-04-03 03:02:03 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+import os
+
+from discord.ext.commands import Bot
+from music import Music
+
+bot = Bot(command_prefix=']')
+token = os.environ.get('KAMBOT')
+
+@bot.command()
+async def hi(ctx):
+    await ctx.send("Hello, %s!" % ctx.author.name)
+
+@bot.event
+async def on_ready():
+    print('Logged in as {0} ({0.id})'.format(bot.user))
+    print('------')
+
+bot.add_cog(Music(bot))
+bot.run(token)
