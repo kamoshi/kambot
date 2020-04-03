dc7ecdf83c42bd1d8b4957ccdd3093930d958a61
kamoshi <18511281+kamoshi@users.noreply.github.com>
2020-04-03 20:20:57 +0200

    update

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 import discord
 import config
 from music import Music
+from core import Core
 
 from discord.ext.commands import Bot
 
@@ -11,6 +12,6 @@ async def on_ready():
     print('Logged in as {0} ({0.id})'.format(bot.user))
     print('------')
 
-# bot.add_cog(cogs.core.Core)
+bot.add_cog(Core(bot))
 bot.add_cog(Music(bot))
 bot.run(config.token)
--- core.py
+++ core.py
@@ -3,3 +3,7 @@ from discord.ext import commands
 class Core(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+
+    @commands.command()
+    async def hi(self, ctx):
+        await ctx.send("Hello, %s!" % ctx.author.name)
