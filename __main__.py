74d0b702ae23be9ca4d560243a7c1135d1966c1e
kamoshi <18511281+kamoshi@users.noreply.github.com>
2020-04-04 15:05:53 +0200

    Added other version of the music bot

--- __main__.py
+++ __main__.py
@@ -1,11 +1,11 @@
 import discord
 import config
 from kambot import Kambot
-from cogs import core, music
+from cogs import core, music_other
 
 bot = Kambot(command_prefix=',')
 
 bot.add_cog(core.Core(bot))
-bot.add_cog(music.Music(bot))
+bot.add_cog(music_other.Music(bot))
 
 bot.run(config.token)
