d9588e179a4fe7e21fc073491478c39f2e81b106
kamoshi <18511281+kamoshi@users.noreply.github.com>
2020-04-09 13:52:11 +0200

    Added avatar command

--- cogs/core.py
+++ cogs/core.py
@@ -1,3 +1,11 @@
+"""
+Commands in this module:
+ - hi
+ - TODO: pfp
+ - TODO: info
+"""
+
+import discord
 from discord.ext import commands
 
 class Core(commands.Cog):
@@ -7,6 +15,16 @@ class Core(commands.Cog):
     @commands.command()
     async def hi(self, ctx):
         await ctx.send("Hello, %s!" % ctx.author.name)
+
+    @commands.command(aliases=["pfp"])
+    async def avatar(self, ctx: commands.Context, *, arg: discord.Member):
+        embed: discord.Embed = discord.Embed(
+            color=discord.Color.blurple(),
+            description="[link]({})".format(arg.avatar_url),
+            title="{}'s avatar".format(arg.display_name)
+        )
+        embed.set_image(url=arg.avatar_url)
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Core(bot))
